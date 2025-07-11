8acf6763fcb4f4894745d7d8a14d34a33c28b744
anjorin001 <anjorinfavour123@gmail.com>
2025-07-11 17:26:07 +0100

    updated the error handler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,11 +2,16 @@ const { ErrorResponse } = require("../util/baseResponse");
 
 const errorHandler = (err, req, res, next) => {
   console.error(err.stack);
-  return new ErrorResponse(
-    res,
-    err.status || 500,
-    err.message || "Internal Server Error"
-  ).send();
+  const status = err.isOperational
+    ? err.statusCode || err.status || 500
+    : 500;
+
+  const message = err.isOperational
+    ? err.message
+    : "Internal Server Error";
+
+  return new ErrorResponse(res, status, message).send();
 };
+
 
 module.exports = errorHandler;
